8db4c9a1dabe048c652c663f6d6608f0a8cd6a61
nguyenhoangnb <nguyenhuyhoang16yl@gmail.com>
2025-10-17 22:12:24 +0700

    fix

--- firmware/hal/usb_manager.py
+++ firmware/hal/usb_manager.py
@@ -11,7 +11,7 @@ class USBManager:
         self.camera_id = camera_id
 
     def is_available(self):
-        # return True
+        return True
         return os.path.ismount(self.path) and os.access(self.path, os.W_OK)
 
     def wait_until_available(self):
